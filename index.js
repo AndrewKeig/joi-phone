7ddc357d6e9b40a9e5079ac6ef6a67b2bfcfa36b
Roman Podlinov <r@thisisagile.com>
2015-08-26 11:27:28 +0300

    Make +(sign plus) optional.
    In accordance with E.164 specification: The presentation of numbers is usually prefixed with the character + (plus sign).

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var Joi = require('joi');
 
 module.exports = {
 	e164 : function () {
-	  return Joi.string().regex(/^\+(?:[0-9] ?){6,14}[0-9]$/);
+	  return Joi.string().regex(/^(\+?)(?:[0-9] ?){6,14}[0-9]$/);
 	}
 }
